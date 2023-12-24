6d3c623d5b1c553ec3006989ef377d5450f8d125
“watchgou” <zjon234@gmail.com>
2023-12-24 19:12:35 +0800

    update

--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ import * as wasm from "../wasm-lib/pkg"
 
 wasm.create_tag();
 
-let result = wasm.get("www.baidu.com");
-console.log(result);
+// wasm.get("http://127.0.0.1:8080/api/v1/simple").then((res) => {
+//     console.log(res);
+// });
+wasm.get("http://127.0.0.1:8080/api/v1/simple");
--- wasm-lib/src/lib.rs
+++ wasm-lib/src/lib.rs
@@ -5,7 +5,7 @@ use wasm_bindgen::prelude::*;
 mod err;
 use err::JsErr;
 use wasm_bindgen_futures::JsFuture;
-use web_sys::{Request, RequestInit};
+use web_sys::{Request, RequestInit, Response};
 
 #[wasm_bindgen]
 pub async fn create_tag() {
@@ -25,10 +25,12 @@ pub async fn create_tag() {
 pub async fn get(url: String) -> Result<JsValue, JsValue> {
     let mut opts = RequestInit::new();
     opts.method("GET");
-    opts.mode(web_sys::RequestMode::Navigate);
+    opts.mode(web_sys::RequestMode::NoCors);
     let request = Request::new_with_str_and_init(&url, &opts)?;
     request.headers().set("Accept", "application/json")?;
     let window = web_sys::window().unwrap();
-    let json = JsFuture::from(window.fetch_with_request(&request)).await?;
+    let resp_value = JsFuture::from(window.fetch_with_request(&request)).await?;
+    let resp: Response = resp_value.dyn_into().unwrap();
+    let json = JsFuture::from(resp.json()?).await?;
     Ok(json)
 }
