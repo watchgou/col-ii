bdab44240a05cef70c416c52c2dbec8b13eb3d4f
“watchgou” <zjon234@gmail.com>
2023-12-24 15:56:13 +0800

    update

--- src/main.js
+++ src/main.js
@@ -2,4 +2,5 @@ import * as wasm from "../wasm-lib/pkg"
 
 wasm.create_tag();
 
-
+let result = wasm.get("www.baidu.com");
+console.log(result);
--- wasm-lib/src/lib.rs
+++ wasm-lib/src/lib.rs
@@ -1,12 +1,14 @@
-use wasm_bindgen::prelude::*;
+use std::ops;
 
+use wasm_bindgen::prelude::*;
 
 mod err;
 use err::JsErr;
+use wasm_bindgen_futures::JsFuture;
+use web_sys::{Request, RequestInit};
 
- 
 #[wasm_bindgen]
-pub fn create_tag() {
+pub async fn create_tag() {
     let document = web_sys::window().unwrap().document().unwrap();
     let symbol = document.query_selector("#root").unwrap();
     if let Some(symbol) = symbol {
@@ -17,4 +19,16 @@ pub fn create_tag() {
         array.set(0, err_msg);
         web_sys::console::log(&array);
     }
+}
+
+#[wasm_bindgen]
+pub async fn get(url: String) -> Result<JsValue, JsValue> {
+    let mut opts = RequestInit::new();
+    opts.method("GET");
+    opts.mode(web_sys::RequestMode::Navigate);
+    let request = Request::new_with_str_and_init(&url, &opts)?;
+    request.headers().set("Accept", "application/json")?;
+    let window = web_sys::window().unwrap();
+    let json = JsFuture::from(window.fetch_with_request(&request)).await?;
+    Ok(json)
 }
