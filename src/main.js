5bc5841955e449f89cb6e27c69b1295c03626e24
“watchgou” <zjon234@gmail.com>
2023-12-26 22:02:47 +0800

    update

--- src/main.js
+++ src/main.js
@@ -6,7 +6,6 @@ wasm.get("http://127.0.0.1:8080/api/v1/simple").then((res) => {
    // console.log(res);
 });
 
-const body = { "name": "test"};
-wasm.post("http://127.0.0.1:8080/api/v1/simple", JSON.stringify(body)).then((res) => {
+wasm.post("http://127.0.0.1:8080/api/v1/simple", JSON.stringify({ "name": "test"})).then((res) => {
     //console.log(res);
 });
