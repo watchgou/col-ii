7b8e7b062fa42526c1e286e36ac751f778d84ef6
“watchgou” <zjon234@gmail.com>
2023-12-26 22:08:20 +0800

    update

--- wasm-lib/src/lib.rs
+++ wasm-lib/src/lib.rs
@@ -8,13 +8,13 @@ use web_sys::{Request, RequestInit, Response};
 #[wasm_bindgen]
 pub async fn create_tag() {
     let document = web_sys::window().unwrap().document().unwrap();
-    let symbol = document.query_selector("#root").unwrap();
+    let symbol = document.query_selector("#root1").unwrap();
     if let Some(symbol) = symbol {
         symbol.set_inner_html("<div><h1>Hello, web-sys!</h1></div>");
     } else {
         let err_msg = JsValue::from(JsErr::TagNotExist.message());
         let array = js_sys::Array::new();
-        array.set(0, err_msg);
+        array.push(&err_msg);
         web_sys::console::log(&array);
     }
 }
