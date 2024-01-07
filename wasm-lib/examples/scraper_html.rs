3515634909d9ecc1f5b7e78a6e01f3150130834c
“watchgou” <zjon234@gmail.com>
2024-01-07 20:17:55 +0800

    update

--- wasm-lib/examples/scraper_html.rs
+++ wasm-lib/examples/scraper_html.rs
@@ -3,15 +3,10 @@ fn main() {
     <div id="div-root">
     <canvas id="canvas-content" style="height: 500px; width: 500px;"></canvas>
     </div>
-"#;
+    "#;
 
     let document = scraper::Html::parse_document(html);
-    let selector = scraper::Selector::parse("body").unwrap();
-    document
-        .select(&selector)
-        .next()
-        .map(|ele| {
-            println!("ele: {:?}", ele.text());
-        })
-        .unwrap();
+    let selector = scraper::Selector::parse("div").unwrap();
+    let x: Vec<scraper::ElementRef> = document.select(&selector).map(|ele| ele).collect();
+    println!("{:#?}", x);
 }
--- wasm-lib/src/enums.rs
+++ wasm-lib/src/enums.rs
@@ -1,15 +1,15 @@
 #[cfg(test)]
 use mockall::{automock, predicate::*};
 pub mod err {
-    pub enum JsErr <'a>{
+    pub enum JsErr<'a> {
         TagNotExist(&'a str),
         ElementCreateFailed(&'a str),
     }
     impl<'a> JsErr<'a> {
         pub fn code(&self) -> String {
             match self {
-                Self::TagNotExist(tar)=>format!("{} tag does not exist",tar),
-                Self::ElementCreateFailed(ele)=>format!("{} create element failed",ele),
+                Self::TagNotExist(tar) => format!("{} tag does not exist", tar),
+                Self::ElementCreateFailed(ele) => format!("{} create element failed", ele),
             }
         }
     }
@@ -27,5 +27,13 @@ mod tests {
         let mut mock: MockFool = MockFool::new();
         mock.expect_foo().with(eq(3)).returning(|x| x + 1);
         assert_eq!(mock.foo(3), 4);
+    }
+
+    #[test]
+    fn test_err() {
+        let tag_not_exist = err::JsErr::TagNotExist("div").code();
+        assert_eq!(tag_not_exist, "div tag does not exist");
+        let element_create_failed = err::JsErr::ElementCreateFailed("body").code();
+        assert_eq!(element_create_failed, "body create element failed");
     }
 }
