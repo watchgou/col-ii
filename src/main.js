403710d76eab9669a58c65bcbf1ddc9121ba737f
“watchgou” <zjon234@gmail.com>
2024-01-14 21:53:27 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,14 +1,12 @@
 import * as util from "./util.js"
 //import * as wgpu from "./wgpu/learn-1.js"
 import * as wgpu from "./wgpu/learn-2.js"
-//import "../main.css"
+import "../main.css"
 
 
 
 util.createElement("#root", "div", "id", "div-content");
 util.createElement("#div-content", "canvas", "id", "canvas-content");
-const id=document.getElementById("canvas-content");
-id.setAttribute("style","height:1000px;width:1000px")
 wgpu.draw();
 
 
